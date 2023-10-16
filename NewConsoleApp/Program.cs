2f966e1e8a26eecbb535f8cca383b29cb6ae74ce
Tamer <tamer.masud@mail.ru>
2023-10-16 13:18:10 +0400

    Добавлены @#

--- NewConsoleApp/Program.cs
+++ NewConsoleApp/Program.cs
@@ -7,6 +7,8 @@
     Console.WriteLine("Привет, C#");
     Console.WriteLine("Мне 25 лет");
     Console.WriteLine("Меня зовут Тамер");
+    Console.WriteLine("\x23");
+    Console.WriteLine("\x40");
 
     Console.ReadKey();
 }
