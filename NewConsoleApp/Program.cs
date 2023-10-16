5fd4c6f55103a7d186ad1be6f307935163e1d73a
Tamer <tamer.masud@mail.ru>
2023-10-16 13:07:38 +0400

    Добавлены новые ввыводы текста.

--- NewConsoleApp/Program.cs
+++ NewConsoleApp/Program.cs
@@ -3,6 +3,11 @@
     const string MyName;
     MyName = "Tamer";
     Console.WriteLine(MyName);
+
+    Console.WriteLine("Привет, C#");
+    Console.WriteLine("Мне 25 лет");
+    Console.WriteLine("Меня зовут Тамер");
+
     Console.ReadKey();
 }
 
