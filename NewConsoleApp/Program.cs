4917c9c5651ba5a5d8ff8f3624ab2677914e46c0
Tamer <tamer.masud@mail.ru>
2023-10-16 15:18:26 +0400

    Убрано все лишнее.

--- NewConsoleApp/Program.cs
+++ NewConsoleApp/Program.cs
@@ -1,37 +1,15 @@
-﻿static void Main(string[] args)
-{
-    const string MyName = "Tamer";
+﻿const string MyName = "Тамер";
 
-    Console.WriteLine(MyName);
+Console.WriteLine("Привет, C#");
+Console.WriteLine("Мне 25 лет");
+Console.WriteLine($"Меня зовут {MyName}");
+Console.WriteLine("\x23");
+Console.WriteLine("\x40");
 
-    Console.WriteLine("Привет, C#");
-    Console.WriteLine("Мне 25 лет");
-    Console.WriteLine("Меня зовут Тамер");
-    Console.WriteLine("\x23");
-    Console.WriteLine("\x40");
-
-    Console.ReadKey();
-}
 
 string myName = "Тамер";
 byte old = 25;
 bool pit = true;
 float foot = 42;
 
-Console.WriteLine(myName);
-Console.WriteLine(old);
-Console.WriteLine(pit);
-Console.WriteLine(foot);
-
-enum DaysOfWeek : byte
-{
-    Monday = 1,
-    Tuesday = 2
-}
-
-enum Semaphore
-{
-    Red = 100,
-    Yellow = 200,
-    Green = 300
-}
+Console.WriteLine(" {0}\n {1}\n {3}\n {2} ", myName, old, foot, pit);
